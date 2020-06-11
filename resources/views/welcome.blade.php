f7b929152f5bd1e3c023565564a7352f10be5a2d
syou2000 <syoutwilight@gmail.com>
2020-06-11 23:01:40 +0900

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<head>
+  <title>Share</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<div class="container ops-main">
+<div class="row">
+  <div class="col-md-12">
+    <h3 class="ops-title">書籍一覧</h3>
+  </div>
+</div>
