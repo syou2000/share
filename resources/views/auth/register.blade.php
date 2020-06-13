53f08bb7941c30c9d2ea0a2a1338e341f4e1a76a
syou2000 <syoutwilight@gmail.com>
2020-06-13 20:59:55 +0900

    welcome page
    ウェルカムページの作成

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
-
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,22 @@
 <head>
   <title>Share</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Khand:wght@500&family=Lobster&family=Pacifico&family=Parisienne&family=Work+Sans:wght@100;200&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
 </head>
-<div class="container ops-main">
-<div class="row">
-  <div class="col-md-12">
-    <h3 class="ops-title">Share</h3>
+<body>
+  <div class="container ops-main">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="ops-title">Share</h1>
+        <div class="loginspace">
+          <a href="/login">ログイン</a>
+          <a href="/register">新規登録</a>
+        </div>
+      </div>
+      <div class="main">
+        <h3 class="message">あなたの<span>瞬間</span>をシェアしませんか?</h3>
+      </div>
+    </div>
   </div>
-  <a href="/register">新規登録</a>
-</div>
+</body>
