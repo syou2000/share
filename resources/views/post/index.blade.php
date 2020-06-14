8f3d992264ae8ffa3c1426e1a04f616faa428585
syou2000 <syoutwilight@gmail.com>
2020-06-14 17:00:57 +0900

    image post
    プレビュー機能と画像投稿機能をつけた

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Share</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/image.js"></script>
+  </head>
+  <body>
+    <div class="view_box">
+      <input type="file" class="file">
+    </div>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('post', 'PostController');
+
+// Route::get('/posts/index', 'PostsController@index');
