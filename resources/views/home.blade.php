8f9c6784c665d0dd83b1aaf15d57dbf5d4393403
syou2000 <syoutwilight@gmail.com>
2020-06-18 11:41:40 +0900

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    すでにログインされています。
                 </div>
             </div>
         </div>
